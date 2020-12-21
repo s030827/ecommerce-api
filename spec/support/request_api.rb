88d56ab6d2243770de71b220d2628a72ac9436a7
Marcus Almeida <s030827@gmail.com>
2020-12-21 17:01:12 -0300

    remove byebug of a line

--- spec/support/request_api.rb
+++ spec/support/request_api.rb
@@ -1,6 +1,5 @@
 module RequestAPI
   def body_json(symbolize_keys: false)
-    byebug
     json = JSON.parse(response.body)
     symbolize_keys ? json.deep_symbolize_keys : json
   rescue StandardError
